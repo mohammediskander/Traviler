f64746d1f023d739b16d0e0d7a15b98e44a5d891
Mohammed Iskander <idarkcry@icloud.com>
2020-12-19 14:09:09 +0300

    Initialize Xcode project

---
+++ Traviler/Traviler/Controllers/HomeController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeController.swift
+//  Traviler
+//
+//  Created by Mohammed Iskandar on 19/12/2020.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    
+    override func loadView() {
+        self.view = HomeView()
+    }
+}
