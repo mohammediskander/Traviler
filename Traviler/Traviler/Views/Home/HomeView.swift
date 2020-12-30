e7dc16e4e52af29adc50451905d9038a26d7cf8f
Mohammed Iskander <idarkcry@icloud.com>
2020-12-30 12:52:19 +0300

    Add HomeView

--- Traviler/Traviler/Views/Home/HomeView.swift
+++ Traviler/Traviler/Views/Home/HomeView.swift
@@ -8,5 +8,22 @@
 import UIKit
 
 class HomeView: UIView {
+    var collectionView: UIView? {
+        didSet {
+            self.setupCollectionView()
+        }
+    }
     
+    func setupCollectionView() {
+        guard let collectionView = collectionView else {
+            return
+        }
+        
+        self.addSubview(collectionView)
+        collectionView.setConstraints([
+            .horizontal(padding: 0),
+            .top(padding: 0, from: nil),
+            .bottom(padding: 0, from: self.layoutMarginsGuide.bottomAnchor)
+        ])
+    }
 }
